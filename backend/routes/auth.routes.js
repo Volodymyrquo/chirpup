29c3e4e0237b2d3e279508ae96c11b04e05f405e
Volodymyr Basok <volodymyrquo@gmail.com>
2024-10-06 16:04:21 +0300

    Auth controller added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,13 @@
-import express from "express"
-import { signup, login, logout } from "../controllers/auth.controller.js"
-const router = express.Router()
+import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
+const router = express.Router();
 
-router.get("/signup", signup)
-router.get("/login", login)
-router.get("/logout", logout)
+import { getMe } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
-export default router
+router.get("/me", protectRoute, getMe);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router;
