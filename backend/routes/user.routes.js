c7a5693800e16ad14bc5a46aae4cb46f0b103fb9
Volodymyr Basok <volodymyrquo@gmail.com>
2024-10-11 11:35:05 +0300

    Add user routes and controllers

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  followUnfollowUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateUser,
+} from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+
+const router = express.Router();
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.put("/update", protectRoute, updateUser);
+export default router;
