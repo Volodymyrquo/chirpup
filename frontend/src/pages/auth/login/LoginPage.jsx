392874573c89670b38cfa121eae9b2d25eed0744
Volodymyr Basok <volodymyrquo@gmail.com>
2024-10-24 11:06:35 +0300

    Auth handled and routes protected in client

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import XSvg from "../../../components/svgs/X";
 
@@ -11,17 +12,46 @@ const LoginPage = () => {
     username: "",
     password: "",
   });
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: loginMutation,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
+    mutationFn: async ({ username, password }) => {
+      try {
+        const res = await fetch("/api/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    loginMutation(formData);
   };
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
-  const isError = false;
 
   return (
     <div className='max-w-screen-xl mx-auto flex h-screen'>
@@ -58,9 +88,9 @@ const LoginPage = () => {
             />
           </label>
           <button className='btn rounded-full btn-primary text-white'>
-            Login
+            {isPending ? "Loading..." : "Login"}
           </button>
-          {isError && <p className='text-red-500'>Something went wrong</p>}
+          {isError && <p className='text-red-500'>{error.message}</p>}
         </form>
         <div className='flex flex-col gap-2 mt-4'>
           <p className='text-white text-lg'>{"Don't"} have an account?</p>
