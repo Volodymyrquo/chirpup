377b8f69a9544bc3b16da1ce97355ff2f0b7f9d4
Volodymyr Basok <volodymyrquo@gmail.com>
2024-10-26 09:56:26 +0300

    Add comment on post mutation

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
@@ -17,7 +18,7 @@ const Post = ({ post }) => {
   const isLiked = post?.likes?.includes(authUser._id);
 
   const isMyPost = authUser._id === post.user._id;
-  const formattedDate = "1h";
+  const formattedDate = formatPostDate(post.createdAt);
   const { mutate: deletePost, isPending: isDeleting } = useMutation({
     mutationFn: async () => {
       try {
@@ -68,7 +69,35 @@ const Post = ({ post }) => {
       toast.error(error.message);
     },
   });
-  const isCommenting = false;
+  const { mutate: commentPost, isPending: isCommenting } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/posts/comment/${post._id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: comment }),
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Comment posted successfully");
+      setComment("");
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
   const handleDeletePost = () => {
     deletePost();
@@ -76,6 +105,8 @@ const Post = ({ post }) => {
 
   const handlePostComment = (e) => {
     e.preventDefault();
+    if (isCommenting) return;
+    commentPost();
   };
 
   const handleLikePost = () => {
@@ -100,7 +131,7 @@ const Post = ({ post }) => {
               className='font-bold'>
               {postOwner.fullName}
             </Link>
-            <span className='text-gray-700 flex gap-1 text-sm'>
+            <span className='text-gray-400 flex gap-1 text-sm'>
               <Link to={`/profile/${postOwner.username}`}>
                 @{postOwner.username}
               </Link>
